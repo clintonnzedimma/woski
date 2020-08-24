887eebee9bc0176bd94acd62372107a05f2931aa
clintonnzedimma <clintonnzedimma@gmail.com>
2020-08-24 01:32:41 +0100

    Fixed Redundant Constructor In Sample Controller

--- app/Controllers/AppController.php
+++ app/Controllers/AppController.php
@@ -3,16 +3,11 @@
  * A sample Controller class
  */
 namespace App\Controllers;
-use Woski\Controller\WoskiController as WoskiController;
+use Woski\Controller\WoskiController;
 
 class AppController extends WoskiController
 {
 	
-	function __construct(){
-		parent::__construct();
-	}
-
-
 
 	public function index() {
 		return function ($req, $res) {
--- index.php
+++ index.php
@@ -1,26 +1,18 @@
 <?php
 
 $_WOSKI_ENV = "development";
-
 require 'woski_autoload.php';
 require 'core/woski.php';
 
 
-
 $app = new Woski\Application;
-
 
 $testRoute = _import("testRoute.php");
 
 $homeController = new App\Controllers\AppController();
 
-
-
 $app->get('/', $homeController->index);
 
-
-
 $app->use("/test",$testRoute);
-
 
 $app->start();
